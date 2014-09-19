80b38f190b004e2381a5624eb91e9b607d9b1179
ava1ar <mail@ava1ar.info>
2014-09-19 01:07:06 -0400

    Slight refactoring and reorganization of the main class

--- src/main/java/net/github/dupsfinder/DupsFinder.java
+++ src/main/java/net/github/dupsfinder/DupsFinder.java
@@ -2,9 +2,6 @@ package net.github.dupsfinder;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-
-import static java.nio.file.FileVisitResult.CONTINUE;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,6 +10,8 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class DupsFinder {
 
@@ -30,48 +29,30 @@ public class DupsFinder {
 		try {
 			final Path rootDirectory = Paths.get(rootDir);
 			System.err.format("Searching for duplicates in the \"%s\" directory.\n", rootDirectory.toRealPath());
-
 			// list of all files to be compared
 			final List<FileEntry> completeFilesList = getFilesList(rootDirectory);
-
-			// prepare duplicateFiles list
+			// process list of files to get the list of lists of files with identical hashsums
 			List<List<FileEntry>> duplicateFilesList = completeFilesList.parallelStream()
-					.collect(Collectors.groupingBy(FileEntry::getSize)).values().parallelStream()
-					// drop groups with single element and group by file partial hashsum
+					// group files by size
+					.collect(Collectors.groupingByConcurrent(FileEntry::getSize)).values().parallelStream()
+					// keep only groups with > 1 elements
 					.filter(p -> p.size() > 1)
 					.flatMap(p -> p.parallelStream())
+					// group remaining files by partial hashsum
 					.collect(Collectors.groupingByConcurrent(FileEntry::getPartialHashSum)).values().parallelStream()
-					// drop groups with single element and group by file hashsum
+					// keep only groups with > 1 elements
 					.filter(p -> p.size() > 1)
 					.flatMap(p -> p.parallelStream())
-					// filter out files entries with enpty partial hashsum (hashsum calculation failed)
+					// filter out files entries with enpty partial hashsum (this means that hashsum calculation failed)
 					.filter(p -> !p.getPartialHashSum().isEmpty())
+					// group files by file hashsum
 					.collect(Collectors.groupingByConcurrent(FileEntry::getHashSum)).values().parallelStream()
-					// drop groups with single element
+					// keep only groups with > 1 elements
 					.filter(p -> p.size() > 1)
+					// store results to the list
 					.collect(Collectors.toList());
-
-			// calculate statistics and prepare output
-			int duplicatesCount = 0;
-			long wastedSpace = 0;
-			final StringBuilder sb = new StringBuilder();
-
-			for (List<FileEntry> files : duplicateFilesList) {
-				final int dupsCount = files.size();
-				long size = 0;
-				for (FileEntry fileEntry : files) {
-					size = fileEntry.getSize();
-					sb.append(fileEntry.setDupsCount(dupsCount)).append('\n');
-				}
-				// update counters
-				duplicatesCount += dupsCount;
-				wastedSpace += (dupsCount - 1) * size;
-			}
-
-			// print output and summary with statistics
-			System.out.print(sb);
-			System.err.format("Examined %s files, found %s dups, total wasted space %s\n",
-					completeFilesList.size(), duplicatesCount, printBytesCount(wastedSpace, false));
+			// calculate summary and print results
+			printResults(completeFilesList.size(), duplicateFilesList);
 		} catch (IOException ex) {
 			System.err.println("WARN " + ex);
 		}
@@ -110,6 +91,39 @@ public class DupsFinder {
 		});
 
 		return fileEntries;
+	}
+
+	/**
+	 * Calculates statistics and prints duplicate files list and summary
+	 *
+	 * @param totalCheckedFilesCount total number of checked files - used in summary output
+	 * @param duplicateFilesList     list of list of duplicate files
+	 */
+	private void printResults(int totalCheckedFilesCount, List<List<FileEntry>> duplicateFilesList) {
+		// calculate statistics and prepare output
+		final StringBuilder sb = new StringBuilder();
+		int duplicatesCount = 0;
+		long wastedSpace = 0;
+		// iterate through all groups of duplicate files
+		for (List<FileEntry> files : duplicateFilesList) {
+			// number of duplicate files in group
+			final int groupDuplicatesCount = files.size();
+			long size = -1;
+			for (FileEntry fileEntry : files) {
+				if (size == -1) {
+					size = fileEntry.getSize();
+				}
+				// append duplicates files list
+				sb.append(fileEntry.setDupsCount(groupDuplicatesCount)).append('\n');
+			}
+			// update counters for duplicate files and wasted space
+			duplicatesCount += groupDuplicatesCount;
+			wastedSpace += (groupDuplicatesCount - 1) * size;
+		}
+		// print output and summary with statistics
+		System.out.print(sb);
+		System.err.format("Examined %s files, found %s dups, total wasted space %s\n",
+				totalCheckedFilesCount, duplicatesCount, printBytesCount(wastedSpace, false));
 	}
 
 	/**
